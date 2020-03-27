74913a7e03ce7cba63ba6fd7b0232d93672aeb43
LiuTong <1515848689@qq.com>
2020-03-27 14:42:46 +0800

    1

--- cuda_project/main.cu
+++ cuda_project/main.cu
@@ -8,16 +8,6 @@
 #define TRUE 1
 using namespace std;
 
-Volume *vol;
-Projection *prj;
-MrcHeader *in_head,*out_head;
-double *x_coef;
-double *y_coef;
-float *prj_real,*vol_real;//pri_real is inputted data ande vol_real is calculated data
-Weight *weight;
-
-int *test;
-
 int PrjXYAngN;
 int vol_pixel_num;
 double iStart,iElaps;
@@ -31,16 +21,9 @@ double cpuSecond(){
 	return (double)tp.tv_sec +(double )tp.tv_usec*1e-6;
 }
 
-__host__ int read_data(char *in_addr,char *angle_addr)
+int read_head_data(Volume *vol,Projection *prj,MrcHeader *in_head,MrcHeader *out_head,char *in_addr)
 {
-	cudaMallocManaged((void **)&vol,sizeof(Volume));
-	cudaMallocManaged((void **)&prj,sizeof(Projection));
-	cudaMallocManaged((void **)&weight,sizeof(weight));
-
-/*****************read head-file ande angle-file*************************************************************/
-	cudaMallocManaged((void **)&in_head,sizeof(MrcHeader));
-	cudaMallocManaged((void **)&out_head,sizeof(MrcHeader));
-	
+/*****************read head-file ande angle-file*************************************************************/	
 	FILE *in_file;
 	in_file = fopen(in_addr,"r");
 	if(!in_file){
@@ -79,16 +62,13 @@ __host__ int read_data(char *in_addr,char *angle_addr)
 	out_head->my=vol->Y;
 	out_head->mz=vol->Z;
 
-	cudaMallocManaged((void **)&test,sizeof(int)*vol->Z);
-	memset(test,0,sizeof(int)*vol->Z);
+	printf("%d 0\n",vol->Z);
+	return TRUE;
+}
 
-	cudaMallocManaged((void **)&x_coef,sizeof(double)*prj->AngN*10);
-	memset(x_coef, 0 , sizeof(double)*prj->AngN*10);
-	printf("%d",sizeof(double)*prj->AngN*10);
 
-	cudaMallocManaged((void **)&y_coef,sizeof(double)*prj->AngN*10);
-	memset(y_coef, 0 , sizeof(double)*prj->AngN*10);
-
+int read_txbr_data(double *x_coef,double *y_coef,char *angle_addr)
+{
 	FILE *angle_file;
 	angle_file = fopen(angle_addr,"r");
 	if(!angle_file){
@@ -97,43 +77,18 @@ __host__ int read_data(char *in_addr,char *angle_addr)
 	}
 	read_coef(x_coef, y_coef, angle_file);
 	fclose(angle_file);
-/***********************************************************************************************************/
+}
 
-/*****************************************read all data*****************************************************/
-	PrjXYAngN = prj->X*prj->Y*prj->AngN;
-	vol_pixel_num = vol->X*vol->Y*vol->Z;
-
-	/*for input file*/
-	cudaMallocManaged((void **)&prj_real,sizeof(float)*PrjXYAngN);
-	memset(prj_real, 0 , sizeof(float)*PrjXYAngN);
-
-	/*for output file*/
-	cudaMallocManaged((void **)&vol_real,sizeof(float)*vol_pixel_num);
-	memset(vol_real, 0 , sizeof(float)*vol_pixel_num);
-	
+int read_all_data(MrcHeader *in_head,float *prj_real,char *in_addr)
+{	
 	FILE *in_context=fopen(in_addr,"r");
 	if(!in_context){
 		printf("Can not open in_context");
 		return FALSE;	
 	}
 	mrc_read_all(in_context,in_head,prj_real);
-	fclose(in_file);
-/*******************************************************************************************************/
-
-	return TRUE;
+	fclose(in_context);
 }
-
-/**
-	Pixel pixel;
-	Weight *comp_prj;
-	comp_prj=(Weight *)malloc(sizeof(Weight));
-	if((comp_prj=(Weight *)malloc(sizeof(Weight)))==NULL)
-	{
-		printf("Error with Function 'read_data()'!Can't malloc memery for 'comp_prj'!");
-		return FALSE;
-	}
-        Slice_backproj_bilinear(pixel, prj, vol, comp_prj, x_coef, y_coef, prj_real,slc_data,Z_start,Z_end,slcN_per); //four-weight 
-**/
 
 /*
 	int sizeofZ_per_block = vol->Z/process_num+1;
@@ -150,83 +105,69 @@ __host__ int read_data(char *in_addr,char *angle_addr)
 	}
 */
 
-__device__ void computeWeight(int x,int y,int z,int angle,double *x_coef,double *y_coef,Weight *weight)
-{
-	double res_x,res_y;
-	int index = 4*angle;
-	res_x = x_coef[index]+x_coef[index+1]*x+x_coef[index+2]*y+x_coef[index+3]*z;
-	res_y = y_coef[index]+y_coef[index+1]*x+y_coef[index+2]*y+y_coef[index+3]*z;	
 
-	weight->x_min = floor(res_x);
-	weight->y_min = floor(res_y);
-
-	weight->x_min_del = res_x - weight->x_min;
-	weight->y_min_del = res_y - weight->y_min;
-}
-
-__global__ void backProjOnGPU(Projection *prj,Volume *vol,double *x_coef,double *y_coef,float *prj_real,float *vol_real,Weight *weight,int *test)
+__global__ void backProjOnGPU(Projection *prj,Volume *vol,double *x_coef,double *y_coef,float *prj_real,float *vol_real,int *test)
 {
 	double divisor;//分子
 	double dividend;//分母
-	int z = threadIdx.x+blockIdx.x*blockDim.x;
-	if(z>=vol->Z) return;
-	int x,y,index,angle,n;
-	printf("Slice %d is beginning.\n",z);
+	int z = threadIdx.x+blockIdx.x*blockDim.x + vol->Zstart;
+	if(z!=vol->Zstart) return;
+	if(z>=vol->Zend) return;
+	int x,y,index,angle,n;	
 	test[z] = 1;
-	for(y=vol->Ystart;y<vol->Yend;y++)
+	for(y=vol->Ystart;y<vol->Ystart+vol->Y;y++)
 	{
-		for(x=vol->Xstart;x<vol->Xend;x++)
+		for(x=vol->Xstart;x<0;x++)
 		{
 			divisor = 0;
 			dividend = 0;
-			index = (x-vol->Xstart)+(y-vol->Ystart)*vol->X;//+(z-vol->Zstart)*vol->X*vol->Y;
-			//要计算的像素在三维结构中的坐标
-			//if(x-vol->Xstart<2&&y-vol->Ystart<2&&index<vol->X)
-			//printf("%d %d %d %d %d\n",index,x,vol->Xstart,y,vol->Ystart);
 			for(angle=0;angle<prj->AngN;angle++)
 			{
-				computeWeight(x,y,z,angle,x_coef,y_coef,weight);
-				//printf("%lf %lf \n",weight->x_min,weight->y_min);
-				if(weight->x_min>=0 && weight->x_min<prj->X && weight->y_min>=0 && weight->y_min<prj->Y)//(x_min,y_min)
+				double res_x,res_y,x_min_del,y_min_del;
+				int id = 4*angle,x_min,y_min;
+				res_x = x_coef[id]+x_coef[id+1]*x+x_coef[id+2]*y+x_coef[id+3]*z;
+				res_y = y_coef[id]+y_coef[id+1]*x+y_coef[id+2]*y+y_coef[id+3]*z;	
+				x_min = floor(res_x);
+				y_min = floor(res_y);
+				x_min_del = res_x - x_min;
+				y_min_del = res_y - y_min;
+				
+				if(x_min>=0 && x_min<prj->X && y_min>=0 && y_min<prj->Y)//(x_min,y_min)
 				{
-					n = weight->x_min + weight->y_min*prj->X + angle*prj->X*prj->Y;
-					divisor += (1-weight->x_min_del)*(1-weight->y_min_del)*prj_real[n];
-					dividend += (1-weight->x_min_del)*(1-weight->y_min_del);
+					n = x_min + y_min*prj->X + angle*prj->X*prj->Y;
+					divisor += (1-x_min_del)*(1-y_min_del)*prj_real[n];
+					dividend += (1-x_min_del)*(1-y_min_del);
 				}
-				if(weight->x_min>=0 && weight->x_min<prj->X && weight->y_min>=0 && weight->y_min<prj->Y)//(x_min+1,y_min)
+				if(x_min>=0 && x_min<prj->X && y_min>=0 && y_min<prj->Y)//(x_min+1,y_min)
 				{
-					n = (weight->x_min+1) + weight->y_min*prj->X + angle*prj->X*prj->Y;
-					divisor += weight->x_min_del*(1-weight->y_min_del)*prj_real[n];
-					dividend += weight->x_min_del*(1-weight->y_min_del);
+					n = (x_min+1) + y_min*prj->X + angle*prj->X*prj->Y;
+					divisor += x_min_del*(1-y_min_del)*prj_real[n];
+					dividend += x_min_del*(1-y_min_del);
 				}
-				if(weight->x_min>=0 && weight->x_min<prj->X && weight->y_min>=0 && weight->y_min<prj->Y)//(x_min,y_min+1)
+				if(x_min>=0 && x_min<prj->X && y_min>=0 && y_min<prj->Y)//(x_min,y_min+1)
 				{
-					n = weight->x_min + (weight->y_min+1)*prj->X + angle*prj->X*prj->Y;
-					divisor += (1-weight->x_min_del)*weight->y_min_del*prj_real[n];
-					dividend += (1-weight->x_min_del)*weight->y_min_del;
+					n = x_min + (y_min+1)*prj->X + angle*prj->X*prj->Y;
+					divisor += (1-x_min_del)*y_min_del*prj_real[n];
+					dividend += (1-x_min_del)*y_min_del;
 				}
-				if(weight->x_min>=0 && weight->x_min<prj->X && weight->y_min>=0 && weight->y_min<prj->Y)//(x_min+1,y_min+1)
+				if(x_min>=0 && x_min<prj->X && y_min>=0 && y_min<prj->Y)//(x_min+1,y_min+1)
 				{
-					n = (weight->x_min+1)+ (weight->y_min+1)*prj->X + angle*prj->X*prj->Y;
-					divisor += weight->x_min_del*weight->y_min_del*prj_real[n];
-					dividend += weight->x_min_del*weight->y_min_del;
+					n = (x_min+1)+ (y_min+1)*prj->X + angle*prj->X*prj->Y;
+					divisor += x_min_del*y_min_del*prj_real[n];
+					dividend += x_min_del*y_min_del;
 				}
 			}
+			index = (x-vol->Xstart)+(y-vol->Ystart)*vol->X+(z-vol->Zstart)*vol->X*vol->Y;
+			printf("%d\n",index);
 			if(dividend!=0.0f)
 			{
 				vol_real[index] = (float)(divisor/dividend);
 				//printf("vol_read[%d]:%f\n",index,vol_real[index]);
 			}
-			
-			//if(19000<index&&index<19010)
-			//if(index<10)
-			//printf("%d-------------%lf\n",index,vol_real[index]);
-		
 		}
 		
 	}
-	test[z] = 2;
-	printf("Slice %d has been finished.\n",z);
+	test[z] = 2;	
 }
 
 void write_data(char *out_addr,MrcHeader *out_head,int Z_end,float *vol_real)
@@ -245,6 +186,13 @@ void write_data(char *out_addr,MrcHeader *out_head,int Z_end,float *vol_real)
 	return;
 }
 
+__global__ void testOnGPU(int *test)
+{
+	int z = threadIdx.x+blockIdx.x*blockDim.x;
+	if(z>=58) return;
+	test[z] = 1;
+}
+
 int main(int argc,char *argv[])
 {
 	iStart = cpuSecond();
@@ -255,17 +203,64 @@ int main(int argc,char *argv[])
 	char* angle_addr = argv[4];
 	//cout<<"arg1:"<<process_num<<" arg2:"<<in_addr<<"  arg3:"<<out_addr<<"  arg4:"<<angle_addr<<endl;
 	
-	read_data(in_addr,angle_addr);
+	Volume *vol;
+	Projection *prj;
+	MrcHeader *in_head,*out_head;
+	double *x_coef;
+	double *y_coef;
+	float *prj_real,*vol_real;//pri_real is inputted data ande vol_real is calculated data
+	int *test;
 
+/*************Head file read ande malloc space*******************/
+	cudaMallocManaged((void **)&vol,sizeof(Volume));
+	cudaMallocManaged((void **)&prj,sizeof(Projection));
+	cudaMallocManaged((void **)&in_head,sizeof(MrcHeader));
+	cudaMallocManaged((void **)&out_head,sizeof(MrcHeader));
+
+	read_head_data(vol,prj,in_head,out_head,in_addr);
+
+/********************************************************************/
+
+
+/*************TXBR file read ande malloc space*******************/
+	cudaMallocManaged((void **)&test,sizeof(int)*vol->Z);
+	memset(test,0,sizeof(int)*vol->Z);
+	cudaMallocManaged((void **)&x_coef,sizeof(double)*prj->AngN*10);
+	memset(x_coef, 0 , sizeof(double)*prj->AngN*10);
+	//printf("%d",sizeof(double)*prj->AngN*10);
+	cudaMallocManaged((void **)&y_coef,sizeof(double)*prj->AngN*10);
+	memset(y_coef, 0 , sizeof(double)*prj->AngN*10);
+
+	read_txbr_data(x_coef,y_coef,angle_addr);
+
+/********************************************************************/
+
+
+/*************Reminding data read ande malloc space*******************/
+
+	PrjXYAngN = prj->X*prj->Y*prj->AngN;
+	vol_pixel_num = vol->X*vol->Y*vol->Z;
+	/*for input file*/
+	cudaMallocManaged((void **)&prj_real,sizeof(float)*PrjXYAngN);
+	memset(prj_real, 0 , sizeof(float)*PrjXYAngN);
+	/*for output file*/
+	cudaMallocManaged((void **)&vol_real,sizeof(float)*vol_pixel_num);
+	memset(vol_real, 0 , sizeof(float)*vol_pixel_num);	
+	read_all_data(in_head,prj_real, in_addr);
+/********************************************************************/
+
+	printf("%d 1\n",vol->Z);
+	//for(int i=0;i<vol->Z;i++ ) printf("%d",test[i]);
 	dim3 block(iLen);
-	printf("%d\n",vol->Z);
 	dim3 grid((vol->Z+block.x-1)/block.x);
 	//cudaDeviceSynchronize();
-	backProjOnGPU<<<grid,block>>>(prj,vol,x_coef,y_coef,prj_real,vol_real,weight,test);	
+	backProjOnGPU<<<grid,block>>>(prj,vol,x_coef,y_coef,prj_real,vol_real,test);	
+	//testOnGPU<<<grid,block>>>(test);
 	cudaDeviceSynchronize();
 	
 	//write_data(out_addr,out_head,vol->Zend-vol->Zstart,vol_real);
 	
+	printf("%d 2\n",vol->Z);
 	for(int qwe = 0;qwe<vol->Z;qwe++) printf("%d",test[qwe]);
 	//cudaDeviceReset();//重置CUDA设备释放程序占用的资源
 
